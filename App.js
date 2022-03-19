68aee0e1a33f8813f4677b409653c126103dfa62
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2022-03-19 16:19:03 -0400

    use PokemonWeather component

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WeatherProvider } from './context/WeatherContext';
+import { PokemonProvider } from './context/PokemonContext';
+
+import PokemonWeather from './PokemonWeather';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WeatherProvider>
+      <PokemonProvider>
+        <PokemonWeather />
+      </PokemonProvider>
+    </WeatherProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
